bb62ebf49bc23ebfdd81ec340dac82254ca40a5f
FIN443 <jjhun1228@gmail.com>
2022-01-11 14:58:30 +0900

    types styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.div`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
   return (
-    <div>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" text="something" />
-    </div>
+    <Container>
+      <H1>hello</H1>
+    </Container>
   );
 }
 
